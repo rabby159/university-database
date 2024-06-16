29a11763d74e51960e46cb781253933f54fe582c
rabby159 <ablazerabbi159@gmail.com>
2024-06-16 22:52:58 +0600

    refector services

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,10 +1,4 @@
 import { StudentModel } from './student.model';
-// import { Student } from './student.interface';
-
-// const createStudentIntoDB = async (student: Student) => {
-//   const result = await StudentModel.create(student);
-//   return result;
-// };
 
 const getAllStudentsFromDB = async () => {
   const result = await StudentModel.find();
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -37,21 +37,4 @@ const userSchema = new Schema<TUser>(
   },
 );
 
-// userSchema.pre('save', async function (next) {
-//   // eslint-disable-next-line @typescript-eslint/no-this-alias
-//   const user = this; // doc
-//   // hashing password and save into DB
-//   user.password = await bcrypt.hash(
-//     user.password,
-//     Number(config.bcrypt_salt_rounds),
-//   );
-//   next();
-// });
-
-// // set '' after saving password
-// userSchema.post('save', function (doc, next) {
-//   doc.password = '';
-//   next();
-// });
-
 export const UserModel = model<TUser>('User', userSchema);
